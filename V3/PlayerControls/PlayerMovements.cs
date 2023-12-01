6b8689d5be516f414a89c191e62445a214ba3ece
SagaeHaruki_ <92028294+SagaeHaruki@users.noreply.github.com>
2023-12-01 20:58:39 +0800

    Add files via upload

--- V3/PlayerControls/PlayerMovements.cs
+++ V3/PlayerControls/PlayerMovements.cs
@@ -2,10 +2,12 @@ using Cinemachine;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Unity.VisualScripting;
 using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
 using UnityEngine.Playables;
 
 public class PlayerMovements : MonoBehaviour
@@ -14,7 +16,7 @@ public class PlayerMovements : MonoBehaviour
     [SerializeField]
     public CharacterController charControl;
     public CinemachineFreeLook freelookCam;
-    public Transform MainCamera;
+    public Transform CameraAngle;
     private bool cameraEnabled = true;
     #endregion
 
@@ -24,7 +26,7 @@ public class PlayerMovements : MonoBehaviour
     [SerializeField]
     private float charSpeed = 1f;
     [SerializeField]
-    public float jumpSpeed = 5.4f;
+    public float jumpSpeed = 6.2f;
     [SerializeField]
     public float gravity = 20.0f;
     [SerializeField]
@@ -35,6 +37,10 @@ public class PlayerMovements : MonoBehaviour
     private bool isWalking;
     [SerializeField]
     private bool isRunning;
+    [SerializeField]
+    private bool isWalkingMoving;
+    [SerializeField]
+    private bool isRunningMoving;
     [SerializeField]
     private bool isSprinting;
     [SerializeField]
@@ -52,11 +58,13 @@ public class PlayerMovements : MonoBehaviour
     #region Variables: Camera Motions
     // Character & Camera
     public float turnSmoothing = 0.1f; // Player direction smoothing
-    public Transform CameraAngle; // Default camera angle
     float smoothingVelocity;
     #endregion
 
     private Animator animator;
+
+    private float jumpTimer = 0f;
+    public float jumpCooldown = 1f;
 
     private void Start()
     {
@@ -86,16 +94,16 @@ public class PlayerMovements : MonoBehaviour
             if (!isHopping)
             {
                 // Calculate the direction of the player
-                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + MainCamera.eulerAngles.y;
-                // This will smoothen the rotation of the angle (More math)
-                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothingVelocity, turnSmoothing);
-                // Rotates the player based on the direction
-                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + CameraAngle.eulerAngles.y;
+            // This will smoothen the rotation of the angle (More math)
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothingVelocity, turnSmoothing);
+            // Rotates the player based on the direction
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-                // Direction base on the camera angle
-                Vector3 newDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-                // This will move the character
-                charControl.Move(newDirection.normalized * charSpeed * Time.deltaTime);
+            // Direction base on the camera angle
+            Vector3 newDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            // This will move the character
+            charControl.Move(newDirection.normalized * charSpeed * Time.deltaTime);
             }
             isMoving = true;
         }
@@ -107,6 +115,11 @@ public class PlayerMovements : MonoBehaviour
 
     private void CheckKeyPressed()
     {
+        if (jumpTimer > 0)
+        {
+            jumpTimer -= Time.deltaTime;
+        }
+
         // Reset the jump flag after the character is grounded again
         if (charControl.isGrounded)
         {
@@ -118,7 +131,7 @@ public class PlayerMovements : MonoBehaviour
         }
 
         // Make the Player Jump
-        if (Input.GetButtonDown("Jump") && !isJumping) // Player can now jump even without moving
+        if (Input.GetButtonDown("Jump") && !isJumping && jumpTimer <= 0) // Player can now jump even without moving
         {
             isJumping = true;
             verticalSpeed = jumpSpeed;
@@ -126,6 +139,7 @@ public class PlayerMovements : MonoBehaviour
             {
                 isHopping = true;
             }
+            jumpTimer = jumpCooldown;
         }
 
         // Running Toggle
@@ -170,29 +184,24 @@ public class PlayerMovements : MonoBehaviour
         {
             if (isRunning)
             {
+                isRunningMoving = true;
+                isWalkingMoving = false;
                 currentState = PlayerState.Running;
                 charSpeed = 3.2f;
             }
-            else
-            {
-            }
-
+            
             if (isWalking)
             {
+                isWalkingMoving = true;
+                isRunningMoving = false;
                 currentState = PlayerState.Walking;
                 charSpeed = 2.4f;
-            }
-            else
-            {
             }
 
             if (isSprinting)
             {
                 currentState = PlayerState.Sprinting;
                 charSpeed = 6.7f;
-            }
-            else
-            {
             }
 
             if (isJumping)
@@ -203,6 +212,9 @@ public class PlayerMovements : MonoBehaviour
         else 
         {
             currentState = PlayerState.Idle;
+            isWalkingMoving = false;
+            isRunningMoving = false;
+            isSprinting = false;
         }
     }
 
@@ -223,24 +235,29 @@ public class PlayerMovements : MonoBehaviour
 
     void FixedUpdate()
     {
-        bool runJumped = animator.GetBool("isJumping");
-        bool runSprint = animator.GetBool("isSprinting");
-        bool walkJumped = animator.GetBool("isJumping");
-        bool walkSprint = animator.GetBool("isSprinting");
-
         // Use the current state to perform actions or behaviors
         switch (currentState)
         {
             case PlayerState.Idle:
-                if (isRunning && !isMoving)
+                if (!isRunningMoving)
                 {
                     animator.SetBool("isRunning", false);
+                    animator.SetBool("isRunningJump", false);
+                    animator.SetBool("isSprinting", false);
                 }
 
-                if (isWalking && !isMoving)
+                if (!isWalkingMoving)
                 {
                     animator.SetBool("isWalking", false);
+                    animator.SetBool("isRunningJump", false);
+                    animator.SetBool("isSprinting", false);
                 }
+
+                if (isSprinting && !isMoving)
+                {
+                    animator.SetBool("isSprinting", false);
+                }
+                // Hopping
 
                 if (isJumping && isWalking && !isMoving || isJumping && isRunning && !isMoving)
                 {
@@ -252,26 +269,65 @@ public class PlayerMovements : MonoBehaviour
                 }
                 break;
             case PlayerState.Walking:
-                if(isWalking && isMoving)
+                if(isWalkingMoving)
                 {
                     animator.SetBool("isRunning", false);
                     animator.SetBool("isWalking", true);
                 }
+
+                if (isWalkingMoving && !isSprinting)
+                {
+                    animator.SetBool("isSprinting", false);
+                }
                 break;
             case PlayerState.Running:
-                if (isRunning && isMoving)
+                if (isRunningMoving)
                 {
                     animator.SetBool("isWalking", false);
                     animator.SetBool("isRunning", true);
                 }
+
+                if (isRunningMoving && !isJumping)
+                {
+                    animator.SetBool("isJumping", false);
+                }
+
+                if (isRunningMoving && !isSprinting)
+                {
+                    animator.SetBool("isSprinting", false);
+                }
                 break;
             case PlayerState.Sprinting:
-                
+                if (isSprinting && isRunningMoving)
+                {
+                    animator.SetBool("isSprinting", true);
+                    animator.SetBool("isRunning", false);
+                }
+                if (isSprinting && isWalkingMoving)
+                {
+                    animator.SetBool("isSprinting", true);
+                    animator.SetBool("isRunning", false);
+                }
+
+                if (isSprinting && !isJumping)
+                {
+                    animator.SetBool("isRunningJump", false);
+                }
                 break;
             case PlayerState.Jumping:
-                if (isWalking && isJumping)
+                if (isWalkingMoving && isJumping)
                 {
                     animator.SetBool("isHopping", true);
+                }
+
+                if (isRunningMoving && isJumping)
+                {
+                    animator.SetBool("isRunningJump", true);
+                }
+
+                if (isSprinting && isJumping)
+                {
+                    animator.SetBool("isRunningJump", true);
                 }
                 break;
             default:
